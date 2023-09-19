0f359168807b49f2c5244359b7c2eb6bb86e58ad
Nadir <nadir@itschittybang.mynetworksettings.com>
2023-09-18 22:04:31 -0400

    Weekly update 18/09/23

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band
 
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html', {'bands': bands})
 
 
+def about(request):
+    return HttpResponse('<h1>About Us</h1> <p>We love merch!</p>')
+
+
+def contact(request):
+    return HttpResponse('<h1>Contact Us</h1> <p>nadir.larhdir@gmail.com</p>')
